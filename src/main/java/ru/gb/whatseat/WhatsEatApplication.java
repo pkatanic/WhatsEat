84d01d4d2f1aa182b8690fd4465a5b7a7e7b2787
RailYunusov <opel808437@gmail.com>
2021-08-27 23:13:24 +0300

    Initial commit

---
+++ src/main/java/ru/gb/whatseat/WhatsEatApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.whatseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsEatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatsEatApplication.class, args);
+    }
+
+}
